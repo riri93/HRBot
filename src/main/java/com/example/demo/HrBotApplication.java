52ade1b4592938be16ab2aa1a03a54ca7c137f5e
rihabkallel <rkallel@hrdatabank.com>
2017-08-21 10:50:12 +0100

    3rd commit

--- src/main/java/com/example/demo/HrBotApplication.java
+++ src/main/java/com/example/demo/HrBotApplication.java
@@ -2,8 +2,15 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "com.example.controller" })
+@EnableJpaRepositories(basePackages = { "com.example.repository" })
+@LineMessageHandler
 public class HrBotApplication {
 
 	public static void main(String[] args) {
