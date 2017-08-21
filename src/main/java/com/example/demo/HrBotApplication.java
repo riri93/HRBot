9087d87e0b54d001740a6e58007933a520bd941c
rihabkallel <rkallel@hrdatabank.com>
2017-08-21 11:38:07 +0100

    com postgres

--- src/main/java/com/example/demo/HrBotApplication.java
+++ src/main/java/com/example/demo/HrBotApplication.java
@@ -3,13 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.example.controller" })
-@EnableJpaRepositories(basePackages = { "com.example.repository" })
 @LineMessageHandler
 public class HrBotApplication {
 
