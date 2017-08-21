cbc3b9eb023fa785577a1ccdf4d8bef376997306
rihabkallel <rkallel@hrdatabank.com>
2017-08-21 10:30:02 +0100

    first commit

---
+++ src/main/java/com/example/demo/HrBotApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrBotApplication.class, args);
+	}
+}
