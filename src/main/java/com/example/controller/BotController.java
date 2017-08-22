5ff285c43dbeef5aaab11c2ac2b3b1965bb8ee6d
rihabkallel <rkallel@hrdatabank.com>
2017-08-22 11:11:14 +0100

    commit

--- src/main/java/com/example/controller/BotController.java
+++ src/main/java/com/example/controller/BotController.java
@@ -1,39 +1,39 @@
-//package com.example.controller;
-//
-//import java.util.HashMap;
-//import java.util.Map;
-//import java.util.UUID;
-//
-//import org.json.JSONException;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
-//import org.springframework.web.bind.annotation.ResponseBody;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//public class BotController {
-//	@RequestMapping("/resource")
-//	public Map<String, Object> home() {
-//		Map<String, Object> model = new HashMap<String, Object>();
-//		model.put("id", UUID.randomUUID().toString());
-//		model.put("content", "Hello World");
-//		return model;
-//	}
-//
-////	@RequestMapping(value = "/webhook", method = RequestMethod.POST)
-////	private @ResponseBody Map<String, Object> webhook(@RequestBody Map<String, Object> obj) throws JSONException {
-////
-////		Map<String, Object> json = new HashMap<String, Object>();
-////
-////		String Speech = "The cost of shipping to  is  for " + obj.get("result");
-////
-////		json.put("speech", " The cost of shipping to  is  1000 euros.");
-////		json.put("displayText", " The cost of shipping to  is  1000 euros.");
-////
-////		json.put("source", "apiai-onlinestore-shipping");
-////		System.out.println("************* ******************" + obj.get("result"));
-////		return json;
-////
-////	}
-//}
+package com.example.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import org.json.JSONException;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BotController {
+	@RequestMapping("/resource")
+	public Map<String, Object> home() {
+		Map<String, Object> model = new HashMap<String, Object>();
+		model.put("id", UUID.randomUUID().toString());
+		model.put("content", "Hello World");
+		return model;
+	}
+
+	@RequestMapping(value = "/webhook", method = RequestMethod.POST)
+	private @ResponseBody Map<String, Object> webhook(@RequestBody Map<String, Object> obj) throws JSONException {
+
+		Map<String, Object> json = new HashMap<String, Object>();
+
+		String Speech = "The cost of shipping to  is  for " + obj.get("result");
+
+		json.put("speech", " The cost of shipping to  is  1000 euros.");
+		json.put("displayText", " The cost of shipping to  is  1000 euros.");
+
+		json.put("source", "apiai-onlinestore-shipping");
+		System.out.println("************* ******************" + obj.get("result"));
+		return json;
+
+	}
+}
