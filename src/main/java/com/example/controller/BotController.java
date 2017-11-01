044156542497fac9242039e33becd8511df144aa
rihabkallel <rkallel@hrdatabank.com>
2017-11-01 15:12:29 +0100

    new bot

--- src/main/java/com/example/controller/BotController.java
+++ src/main/java/com/example/controller/BotController.java
@@ -6,20 +6,14 @@ import java.util.UUID;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.repository.SalonRepository;
-
 @RestController
 public class BotController {
-
-	@Autowired
-	SalonRepository salonRepository;
 
 	@RequestMapping("/resource")
 	public Map<String, Object> home() {
